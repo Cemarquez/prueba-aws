0c08d196a8830cfac3ca279d1e1d9e2c4277112d
César E Márquez <cesare.marquezm@uqvirtual.edu.co>
2021-10-13 08:32:28 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cambios para el curso
         </p>
         <a
           className="App-link"
